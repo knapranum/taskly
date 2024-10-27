976ea3dcda306cfb000fd2adc5953d82bbfece5a
Peter Knap <knap.ranum@gmail.com>
2024-10-27 17:09:19 +0100

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "It will be gone for good!",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, deleting."),
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+    );
+
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+        
+      <View
+        style={styles.itemContainer}
+      >
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          style={styles.button} 
+          onPress={() => handleDelete()} 
+          activeOpacity={0.4}
+          >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>    
+      </View>
     </View>
   );
 }
@@ -13,8 +42,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.colorWhite,
+    justifyContent: "center",
   },
+  itemContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    borderBottomColor: theme.colorCerulean,
+    borderBottomWidth: 2,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  itemText: { 
+    fontSize: 18, 
+    fontWeight: "400", 
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  }
 });
---
+++ theme.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export const theme = {
+  colorCerulean: "#1a759f",
+  colorWhite: "#fff",
+  colorBlack: "#000",
+};
